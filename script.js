de546d3baa6f32e7eb4aa0267e07a9d6616a9278
DESKTOP-DL4CPBN\Gabi <gomesg@usc.edu>
2024-11-01 20:56:12 +0100

    Create threeJS playground to practice lessons.

---
+++ script.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+//Scene
+const scene = new THREE.Scene();
+
+//Camera
+const sizes = {width: 800, height: 600};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector('canvas.webgl');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setClearColor(0xf5f5dc); // Beige color
+
+//Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x800080});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Outline
+const edges = new THREE.EdgesGeometry(geometry);
+const lineMaterial = new THREE.LineBasicMaterial({color: 0xccff66});
+const outline = new THREE.LineSegments(edges, lineMaterial);
+scene.add(outline);
+
+//Render
+renderer.render(scene, camera);
